f72a4c64466da527b986e9620ccb084af45216d1
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2018-12-14 15:13:34 +0200

    added instructions

---
+++ javacodes.js
@@ -0,0 +1,7 @@
+var sideSum1 = function(firstvalue, secondvalue) {
+  return firstvalue + secondvalue;
+}
+;
+var sideSum2 = function(secondvalue, thirdvalue) {
+  return secondvalue + thirdvalue;
+};
